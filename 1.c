67fc275b2d35f9e2a3225eafc4aa8233151185c3
rakhmatov.sp <44374413+ssssunat@users.noreply.github.com>
2020-12-06 13:02:46 +0300

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,26 @@
+include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <time.h>
+
+int main(int argc, char *argv[]) {
+	if (argc != 2) {
+		printf ("Usage: %s some.file\n", argv[0]);
+		return 1;
+	}
+
+	struct stat stat_buf;
+	if (lstat (argv[1], &stat_buf) == -1) {
+		perror("Failed to stat");
+		return 2;
+	}
+
+	printf("Mode: %06o\n", stat_buf.st_mode);
+	printf("Size: %llu\n", (unsigned long long)stat_buf.st_size);
+	printf("Used space: %llu\n", (unsigned long long)stat_buf.st_blocks * S_BLKSIZE);
+	printf("Last status change:       %s", ctime(&stat_buf.st_ctime));
+  printf("Last file access:         %s", ctime(&stat_buf.st_atime));
+  printf("Last file modification:   %s", ctime(&stat_buf.st_mtime));
+	return 0;
+}
