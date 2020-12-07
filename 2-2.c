2c148a55b64a0498ffb89af6dc6f68905aa8ef06
Rakhmatov Sunat <44374413+ssssunat@users.noreply.github.com>
2020-12-07 12:30:20 +0300

    Update 2-2.c

--- 2-2.c
+++ 2-2.c
@@ -6,25 +6,21 @@
 
 int main(int argc, char *argv[]) {
 	int fd, cfd = 0;
-    	if (argc != 3) {
+    if (argc != 3) {
 		printf ("Incorrect Usage");
 		return 1;
 	 }
-	 fd = open(argv[1], O_APPEND | O_RDWR | O_CREAT, S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH);
-	 if (fd == -1) {
-		   perror("Cannot create or modify file\n");
-		   return 1;
+	fd = open(argv[1], O_APPEND | O_RDWR | O_CREAT, S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH);
+	if (fd == -1) {
+		perror("Cannot create or modify file\n");
+		return 1;
 	 }
-   int bytes = 1;
-   dprintf(fd,"%s", argv[2]);
-	 if (bytes == -1) {
+    int bytes = 1;
+    dprintf(fd,"%s", argv[2]);
+	if (bytes == -1) {
 	     perror("error");
-       return 1;
+             return 1;
 	}
-	if ((int) bytes <  size) {
-    		printf ("Writing is unsucessful\n");
-    	}
-  
 	cfd = close(fd);
 	if (cfd != 0) {
 		 perror("Unsucessful closing");
