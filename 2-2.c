d8ce8c98dc88a44798a8883271880efb98aa9d69
Rakhmatov Sunat <44374413+ssssunat@users.noreply.github.com>
2020-12-07 12:43:31 +0300

    Update 2-2.c

--- 2-2.c
+++ 2-2.c
@@ -1,32 +1,27 @@
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <stdio.h> 
+#include <stdio.h>
 #include <unistd.h>
 #include <string.h>
 
 int main(int argc, char *argv[]) {
-	int fd, cfd = 0;
-    if (argc != 3) {
-		printf ("Incorrect Usage");
-		return 1;
-	 }
-	fd = open(argv[1], O_APPEND | O_RDWR | O_CREAT, S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH);
-	if (fd == -1) {
-		perror("Cannot create or modify file\n");
-		return 1;
-	 }
-    int bytes = 1;
-    dprintf(fd,"%s", argv[2]);
-	if (bytes == -1) {
-	     perror("error");
-             return 1;
-	}
-	cfd = close(fd);
-	if (cfd != 0) {
-		 perror("Unsucessful closing");
-    		return 1;
-	}
-	
-	return 0;
+        int fd, cfd = 0;
+        if (argc != 3) {
+                printf ("Incorrect Usage");
+                return 1;
+         }
+         fd = open(argv[1], O_APPEND | O_RDWR | O_CREAT, S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH);
+         if (fd == -1) {
+                   perror("Cannot create or modify file\n");
+                   return 1;
+         }
+         dprintf(fd,"%s", argv[2]);
+        cfd = close(fd);
+        if (cfd != 0) {
+                 perror("Unsucessful closing");
+                return 1;
+        }
+
+        return 0;
 
 }
