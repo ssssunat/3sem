1af0d556dd8f2890416be8657c271bb4433d2a8f
Rakhmatov Sunat <44374413+ssssunat@users.noreply.github.com>
2020-12-17 13:03:11 +0300

    Update 3-2.c

--- 3-2.c
+++ 3-2.c
@@ -5,67 +5,64 @@
 #include <stdio.h>
 #include <stdint.h>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
 	if (argc != 3){
 		printf("Usage: %s [source file] [target file]\n", argv[0]);
 		return 1;
 	}
-    struct stat stat_buf;
-    if (lstat(argv[1], &stat_buf) == -1){
+    struct stat filestat;
+    if (lstat(argv[1], &filestat) == -1) {
         perror("Failed to stat");
         return 2;
     }
-    if ((stat_buf.st_mode & S_IFMT) != S_IFREG)
-    {
+    if ((filestat.st_mode & S_IFMT) != S_IFREG) {
         printf("Error: Not regular\n");
         return 2;
     }
 
-	int src_fd = open(argv[1], O_RDONLY);
-	if (src_fd == -1){
+	int file1_fd = open(argv[1], O_RDONLY);
+	if (file1_fd == -1){
 		perror("Failed to open source file");
 		return 2;
 	}
-	int dst_fd = open(argv[2], O_WRONLY|O_CREAT|O_TRUNC, S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH);
-	if (dst_fd == -1){
+	int file2_fd = open(argv[2], O_WRONLY|O_CREAT|O_TRUNC, S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH);
+	if (file2_fd == -1){
 		perror("Failed to open target file");
 		return 2;
 	}
 	off_t bytes_read = 0;
-    off_t all_bytes_written = 0;
+    	off_t all_bytes_written = 0;
 	while (1){
-		uint8_t buf[4096];
-		ssize_t buf_size = pread(src_fd, buf, sizeof(buf), bytes_read);
+		char buf[512];
+		ssize_t buf_size = pread(file1_fd, buf, sizeof(buf), bytes_read);
 
-		if (buf_size == -1){
+		if (buf_size == -1) {
 			perror("Failed to read a block");
-			close(src_fd);
-			close(dst_fd);
-            return 2;
+			close(file1_fd);
+			close(file2_fd);
+            		return 2;
 		}
 		if (buf_size == 0){
 			break;
 		}
-        bytes_read += buf_size;
+       		bytes_read += buf_size;
 		size_t local_buf_size = (size_t)buf_size;
 		size_t bytes_written = 0;
-		while (bytes_written < local_buf_size){
-			ssize_t write_result = pwrite(dst_fd, &buf[bytes_written], local_buf_size - bytes_written, all_bytes_written);
+		while (bytes_written < local_buf_size) {
+			ssize_t write_result = pwrite(file2_fd, &buf[bytes_written], local_buf_size - bytes_written, all_bytes_written);
 
-			if (write_result == -1){
+			if (write_result == -1) {
 				perror("Failed to write");
-				close(src_fd);
-				close(dst_fd);
+				close(file1_fd);
+				close(file2_fd);
 				return 3;
 			}
 			bytes_written += (size_t)write_result;
-            all_bytes_written += (off_t)write_result;
-		}
-			
+            		all_bytes_written += (off_t)write_result;
+		}		
 	}
-    fsync(dst_fd);
-	close(src_fd);
-	close(dst_fd);
+    	fsync(file2_fd);
+	close(file1_fd);
+	close(file1_fd);
 	return 0;
 }
